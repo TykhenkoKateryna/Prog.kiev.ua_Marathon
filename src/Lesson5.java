05163d315de6e8a32911ce3790fee6eba7f651c0
@TykhenkoKateryna <kateryna.sn4@gmail.com>
2020-03-11 22:45:43 +0200

    add task: get numbers from 0 to 50 through different loops

--- src/Lesson5.java
+++ src/Lesson5.java
@@ -1,15 +1,24 @@
 import java.util.Scanner;
-
+//task: to print digits from 0 to 50 with different loops
 public class Lesson5 {
 
 	public static void main(String[] args) {
-	int a=0;
+		int a = 0;
 		while (a < 50) {
-			
+
 			a++;
 			System.out.println(a);
 		}
-		
+		for (int i = 0; i <= 50; i++) {
+			System.out.println(i);
+		}
+
+		int d;
+		d = 0;
+		do {
+			d++;
+			System.out.println(d);
+		} while (d < 50);
 
 	}
 
